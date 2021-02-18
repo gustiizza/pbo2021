fa3fa11bd1dba054a513110d7294175dbe8da2ed
gustiizza <gustiizza.64@gmail.com>
2021-02-18 19:35:22 +0700

    pratikum 02 membuat objek dan class

---
+++ Mahasiswa.php
@@ -0,0 +1,17 @@
+<?php
+class Mahasiswa
+{
+//atribut
+    public $nama;
+    public $nim;
+    public $tanggal_lahir;
+    public $jenis_kelamin;
+//method 1
+    public function tampilkanAngkatan (){
+        
+    }
+//method 2
+    public function tampilkanUmur(){
+
+    }
+}
---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+include ("Mahasiswa.php");
+
+//objek 1
+$gusti = new Mahasiswa();
+$gusti->nama = "Gusti Izza Maulana";
+$gusti->nim = "H1101191073";
+$gusti->tanggal_lahir = "15-08-2000";
+$gusti->jenis_kelamin = "Laki-laki";
+
+//objek 2
+$fahrul = new Mahasiswa();
+$fahrul->nama = "Fachrul Ramadhanu";
+$fahrul->nim = "H1101191057";
+$fahrul->tanggal_lahir = "12-01-2001";
+$fahrul->jenis_kelamin = "Laki-laki";
+
+//objek 3
+$mada = new Mahasiswa();
+$mada->nama = "Ramadhan";
+$mada->nim = "H1101191035";
+$mada->tanggal_lahir = "22-03-2001";
+$mada->jenis_kelamin = "Laki-laki"; 
